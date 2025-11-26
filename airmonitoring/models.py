1736b8a03f873b178531462e5c04a7baa2293568
tancredt <nicholasrperkins@gmail.com>
2025-11-27 06:57:47 +1100

    modify for Job test

--- airmonitoring/models.py
+++ airmonitoring/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Job(models.Model):
+  label = models.CharField(max_length=16)
+  notes = models.CharField(max_length=256)
+  commencement_date = models.DateField()
+  suburb = models.CharField(max_length=32)
+  complete = models.BooleanField(default=False)
+    
+  class Meta:
+    ordering = ['-commencement_date']  
+
+  def __str__(self):
+    return self.label
--- airmonitoring/views.py
+++ airmonitoring/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import SessionAuthentication
 
-# Create your views here.
+from .models import (
+  Job
+)
+from .serializers import (
+  JobSerializer
+)
+
+class JobViewSet(viewsets.ModelViewSet):
+  queryset = Job.objects.all()
+  serializer_class = JobSerializer
+  authentication_classes = [SessionAuthentication]
+  permission_classes = [permissions.IsAuthenticated]
+
+    
+
+
