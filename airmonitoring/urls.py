9dded442d8e2b3b1bc019af52746d14b26377cf4
tancredt <nicholasrperkins@gmail.com>
2025-11-27 08:55:33 +1100

    modify for Job test

--- airmonitoring/urls.py
+++ airmonitoring/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-  path('jobs/', views.JobViewSet.as_view({'get': 'list', 'put': 'update', 'post': 'create'), name='jobs'),
+  path('jobs/', views.JobViewSet.as_view({'get': 'list', 'put': 'update', 'post': 'create'}), name='jobs'),
 ]
